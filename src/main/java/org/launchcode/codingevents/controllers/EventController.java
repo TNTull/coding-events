27224cb4b7ede4b73b6d14ef73a1138ec591ce2a
Teresa <teresatull@comcast.net>
2022-10-30 17:42:41 -0500

    java ch 14 exercises done

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,7 +1,10 @@
 //Teresa Tull worked on this on 10-22-2022 ch 13 work and exercises
+// 10-24-2022 work on ch 14
 
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +23,6 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
-
     @GetMapping
     public String displayAllEvents(Model model) {
 //        List<String> events = new ArrayList<>();
@@ -29,14 +30,14 @@ public class EventController {
 //        events.add("Strange Loop");
 //        events.add("Apple WWDC");
 //        events.add("SpringOne Platform");
-        //above is taken out for create exercise wanted a hashmap
-        HashMap<String, String> events = new HashMap<>();
-        events.put("Menteaship", "A fun meetup for connecting with mentors");
-        events.put("Code With Pride", "A fun meetup sponsored by LaunchCode");
-        events.put("Javascripty", "An imaginary meetup for Javascript developers");
+        //above is taken out for create, exercise wanted a hashmap taken out for ch 14
+//        HashMap<String, String> events = new HashMap<>();
+//        events.put("Menteaship", "A fun meetup for connecting with mentors");
+//        events.put("Code With Pride", "A fun meetup sponsored by LaunchCode");
+//        events.put("Javascripty", "An imaginary meetup for Javascript developers");
 
         model.addAttribute("title", "All Events");
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -47,8 +48,8 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
+        EventData.add(new Event(eventName, eventDescription));
         return "redirect:";
     }
 
